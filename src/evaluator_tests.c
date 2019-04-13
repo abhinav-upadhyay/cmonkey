b3657ffa83417d7422fa593bf8f4ff192f95e15c
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2019-04-13 19:05:44 +0530

    Evaluate infix boolean expressions as well

--- src/evaluator_tests.c
+++ src/evaluator_tests.c
@@ -100,7 +100,15 @@ test_eval_bool_expression(void)
 
     test_input tests[] = {
         {"true", true},
-        {"false", false}
+        {"false", false},
+        {"1 < 2", true},
+        {"1 > 2", false},
+        {"1 < 1", false},
+        {"1 > 1", false},
+        {"1 == 1", true},
+        {"1 != 1", false},
+        {"1 == 2", false},
+        {"1 != 2", true}
     };
 
     print_test_separator_line();
