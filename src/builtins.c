249e52e856b7d49eed2532e15cd2a7a3a123ddd0
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2019-04-23 15:22:14 +0530

    Add support for arrays in len

--- src/builtins.c
+++ src/builtins.c
@@ -7,19 +7,25 @@
 static monkey_object_t *
 len(cm_list *arguments)
 {
+    monkey_string_t *str;
+    monkey_array_t *array;
     if (arguments->length != 1) {
         return (monkey_object_t *) create_monkey_error(
             "wrong number of arguments. got=%zu, want=1", arguments->length);
     }
 
     monkey_object_t *arg = (monkey_object_t *) arguments->head->data;
-    if (arg->type != MONKEY_STRING) {
-        return (monkey_object_t *) create_monkey_error(
-            "argument to len not supported, got %s", get_type_name(arg->type));
+    switch (arg->type) {
+        case MONKEY_STRING:
+            str = (monkey_string_t *) arg;
+            return (monkey_object_t *) create_monkey_int(str->length);
+        case MONKEY_ARRAY:
+            array = (monkey_array_t *) arg;
+            return (monkey_object_t *) create_monkey_int(array->elements->length);
+        default:
+            return (monkey_object_t *) create_monkey_error(
+                "argument to len not supported, got %s", get_type_name(arg->type));
     }
-
-    monkey_string_t *str = (monkey_string_t *) arg;
-    return (monkey_object_t *) create_monkey_int(str->length);
 }
 
 static char *
