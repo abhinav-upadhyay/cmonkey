ef73527f9782944d50de11cd9a5599e7080708af
Abhinav Upadhyay <er.abhinav.upadhyay@gmail.com>
2019-10-26 23:34:10 +0530

    Missed committing two test files

---
+++ src/object_test_utils.h
@@ -0,0 +1,10 @@
+#ifndef OBJECT_TEST_UTILS_H
+#define OBJECT_TEST_UTILS_H
+#include "object.h"
+
+void test_monkey_object(monkey_object_t *, monkey_object_t *);
+void test_null_object(monkey_object_t *);
+void test_integer_object(monkey_object_t *, long);
+void test_boolean_object(monkey_object_t *, _Bool);
+
+#endif
